7eab8696ed3fe0733c32a2aa58552a735d41de6c
Artem Shoobovych <shybovycha@gmail.com>
2015-03-30 00:34:46 +0200

    Removed trashy code

--- ski_rent/ski_rent/userdao.cpp
+++ ski_rent/ski_rent/userdao.cpp
@@ -19,10 +19,6 @@ QList<User> UserDAO::getAllUsers() {
 }
 
 QList<User> UserDAO::findUsers(QString query) {
-    /*QString likeCondition = QString("'%%%1%%'").arg(QString(query).split("\\s+").join("%"));
-    QString whereCondition = QString("name LIKE '%1' OR surname LIKE %1 OR document_number LIKE %1 OR country LIKE %1 OR city LIKE %1 OR address LIKE %1 OR phone LIKE %1").arg(likeCondition);
-    QString sql = QString("SELECT * FROM users WHERE %1").arg(whereCondition);*/
-
     QString sql = QueryBuilder::getSearchQuery<User>(query);
 
     QList<DBRow> rows = this->getDb()->select(sql);
@@ -32,8 +28,6 @@ QList<User> UserDAO::findUsers(QString query) {
 }
 
 User UserDAO::getUser(unsigned int id) {
-    //QString query = QString("SELECT * FROM users WHERE id = %1;").arg(id);
-
     QString sql = QueryBuilder::getReadQuery<User>(id);
 
     QList<DBRow> rows = this->getDb()->select(sql);
@@ -46,18 +40,12 @@ User UserDAO::getUser(unsigned int id) {
 }
 
 void UserDAO::updateUser(User newUserData) {
-    /*QString query = QString("UPDATE users SET name = '%2', surname = '%3', document_type = '%4', document_number = '%5', country = '%6', city = '%7', address = '%8', phone = '%9' WHERE id = %1")
-            .arg(newUserData.getId())
-            .arg(newUserData.getName(), newUserData.getSurname(), newUserData.getDocumentType(), newUserData.getDocumentNumber())
-            .arg(newUserData.getCountry(), newUserData.getCity(), newUserData.getAddress(), newUserData.getPhone());*/
-
     QString sql = QueryBuilder::getCreateQuery<User>(newUserData);
 
     this->getDb()->update(sql);
 }
 
 void UserDAO::deleteUser(unsigned int id) {
-    //QString query = QString("DELETE FROM users WHERE id = %1;").arg(id);
     QString sql = QueryBuilder::getDeleteQuery<User>(id);
 
     this->getDb()->update(sql);
