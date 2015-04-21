bf9e1f11cabc937a1f6f450d20659fa298c6ac68
Artem Shoobovych <shybovycha@gmail.com>
2015-04-21 10:59:51 +0200

    Started reworking querybuilders

--- ski_rent/ski_rent/query_builders/equipmentquerybuilder.cpp
+++ ski_rent/ski_rent/query_builders/equipmentquerybuilder.cpp
@@ -7,27 +7,12 @@
 template<>
 QueryBuilder<Equipment>::QueryBuilder()
 {
-    this->tableName = "equipment";
-
-    this->searchColumns.append("type");
-    this->searchColumns.append("amount");
-    this->searchColumns.append("condition");
-
-    this->updateColumns.append("type");
-    this->updateColumns.append("amount");
-    this->updateColumns.append("condition");
 }
 
 template<>
 QString QueryBuilder<Equipment>::getSearchQuery(QString query) {
     QString likeCondition = QString("'%%1%'").arg(QString(query).split("\\s+").join("%"));
-    QStringList likes;
-
-    for (int i = 0; i < this->searchColumns.size(); i++) {
-        likes.append(QString("`%1` LIKE %2").arg(this->searchColumns[i]).arg(likeCondition));
-    }
-
-    QString sql = QString("SELECT * FROM `available_equipment` WHERE %1").arg(likes.join(" OR "));
+    QString sql = QString("CALL quickSearchEquipment(%1, @err)").arg(likeCondition);
 
     return sql;
 }
